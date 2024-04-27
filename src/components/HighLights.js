c6caf16081d1b31ecee0575efecfcc6e259d1f8d
Jyotiranjan <sahoorinku63@gmail.com>
2024-04-27 16:40:20 +0530

    fixed the mapping error

--- src/components/HighLights.js
+++ src/components/HighLights.js
@@ -1,15 +1,16 @@
 import React from "react";
 
-function HighLights() {
+function HighLights({ stats }) {
   return (
     <>
       <div className="bg-slate-600 p-2 text-slate-200 flex flex-col justify-start items-center">
-        <h2 className="text-sm mt-2">Wind Status</h2>
-        <div className="mt-2">
-          <span className="text-4xl font-bold">2.4</span>
-          <span className="text-2xl"> mph</span>
+        <h2 className="text-sm mt-2">{stats.title}</h2>
+        <div className=" w-fit mt-2">
+          <span className=" text-4xl font-bold">{stats.value}</span>
+          <span className="text-2xl"> {stats.unit}</span>
         </div>
-        {/* <div className="mt-4">
+        {
+          stats.direction ? (<div className="mt-2 flex">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -24,15 +25,16 @@ function HighLights() {
               d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
             />
           </svg>
-          <div className="ms-2 text-slate-200">E</div>
-        </div> */}
+          <div className="ms-2 text-slate-200">{stats.direction}</div>
+        </div>) : null
+          }
 
-        <div className="w-full mt-4 bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700">
+        {stats.title == "Humidity" ? (<div className="w-full mt-4 bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700">
           <div
             className="bg-blue-600 h-1.5 rounded-full dark:bg-blue-500"
-            style={{ width: "45%" }}
+            style={{ width: `${stats.value}%` }}
           ></div>
-        </div>
+        </div>):null}
       </div>
     </>
   );
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -3,7 +3,7 @@ import Temperature from "./Temperature";
 import HighLights from "./HighLights";
 const MainPage = () => {
   const [city, setCity] = useState("Bhubaneswar");
-  const [weatherData, setWeatherData] = useState(null);
+  const [weatherData, setWeatherData] = useState([]);
   const apiURL = `https://api.weatherapi.com/v1/current.json?key=75d96e06e05843ff8b8165516241904&q=${city}&aqi=no`;
 
   useEffect(() => {
@@ -16,36 +16,62 @@ const MainPage = () => {
       })
       .then((data) => {
         setWeatherData(data);
-        console.log(weatherData.current.temp_c);
       })
       .catch((e) => {
         console.log(e.message);
       });
-  },[]);
+  }, [city]);
 
-//   console.log(weatherData);
+  console.log(weatherData?.current);
+
   return (
     <div className="bg-[#1f213A] align-top h-screen flex justify-center">
       <div className="w-1/5 h-1/3 mt-44">
-        <Temperature 
-        setCity={setCity}
-        // stats={{
-        //     temp:weatherData.current.temp_c,
-        //     condition : weatherData.current.condition.text,
-        //     isDay : weatherData.current.is_day,
-        //     location : weatherData.location.name,
-        //     time : weatherData.location.localtime
-        // }} 
+        <Temperature
+          setCity={setCity}
+          stats={{
+            temp: weatherData?.current?.temp_c,
+            condition: weatherData?.current?.condition.text,
+            isDay: weatherData?.current?.is_day,
+            location: weatherData?.location?.name,
+            time: weatherData?.location?.localtime,
+          }}
         />
       </div>
       <div className="w-1/3 h-1/3 mt-44 p-10 grid grid-cols-2 gap-6">
         <h2 className="text-2xl text-slate-200 col-span-2">
           Today's Highlights
         </h2>
-        <HighLights />
-        <HighLights />
-        <HighLights />
-        <HighLights />
+        {weatherData && (
+          <>
+            <HighLights
+            stats= {{
+              title: "Wind Status" ,
+              value : weatherData?.current?.wind_mph,
+              unit: "mph",
+              direction : weatherData?.current?.wind_dir
+            }} 
+            />
+            <HighLights
+             stats= {{
+              title: "Humidity" ,
+              value : weatherData?.current?.humidity,
+              unit: "%",
+            }} />
+            <HighLights 
+             stats= {{
+              title: "Visibility" ,
+              value : weatherData?.current?.vis_miles,
+              unit: "miles",
+            }}/>
+            <HighLights 
+             stats= {{
+              title: "Air Pressure" ,
+              value : weatherData?.current?.pressure_mb,
+              unit: "mb",
+            }} />
+          </>
+        )}
       </div>
     </div>
   );
